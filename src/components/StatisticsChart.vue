bb78cfdbe1279de50afc985ec2d5f5e2d214ee53
paulwoojungkim <wj2kim@gmail.com>
2020-03-21 19:42:06 +0900

    third commit

--- src/components/StatisticsChart.vue
+++ src/components/StatisticsChart.vue
@@ -1,36 +1,90 @@
 <template>
 
   <div class ="area">
-
     <form class="col s12">
       <div class="row">
         <!-- temperature input -->
-        <div class = "input-field col-s4">
+        <div class = "input-field col s3">
           <input id='temp' type = 'number' class='validate'/>
           <label for = 'temp'>Temperature...</label>
         </div>
 
         <!-- humidity input -->
-        <div class = "input-field col-s4">
+        <div class = "input-field col s3">
           <input id='hum' type = 'number' class='validate'/>
           <label for = 'hum'>Humidity...</label>
         </div>
 
         <!-- humidity input -->
-        <div class = "input-field col-s4">
+        <div class = "input-field col s3">
           <input id='pot' type = 'number' class='validate'/>
           <label for = 'pot'>Potentio...</label>
+        </div>
+
+        <div class = "col s3">
+          <div class ="row">
+
+            <!-- get button -->
+            <a class ="col s6 waves-effect waves-light btn-large teal lighten-2">
+              <i class="meterial-icons">
+                cloud_download
+              </i>
+            </a>
+
+            <!-- post button -->
+            <a class ="col s6 waves-effect waves-light btn-large pink lighten-2">
+              <i class="meterial-icons">
+                cloud_upload
+              </i>
+            </a>
+          </div>
         </div>
       </div>
     </form>
 
+    <!-- chart.js component -->
+    <LineChart
+      :chart-data = 'datacollection'
+    >
+    </LineChart>
   </div>
 
 </template>
 
 <script>
-export default {
+import LineChart from './StatisticsLine.js'
 
+export default {
+  components : {
+    LineChart
+  },
+  data(){
+    return {
+      datacollection : null,
+      loaded : false
+    }
+  },
+  mounted(){
+    this.fillData()
+  },
+  methods : {
+    fillData(){
+      this.datacollection = {
+        labels : [0,1,2,3,4,5,6,7,8,9],
+        datasets : [
+          {
+            label : 'Temp (°C)',
+            backgroundColor : 'rgba(255,0,0,0.2)',
+            borderColor : 'lightblue',
+            pointBackgroundColor : 'red',
+            borderWidth: 1,
+            pointBorderColor : 'red',
+            data : [0,1,2,3,4,5,6,7,8,9]
+          }
+        ]
+      }
+    }
+  }
 }
 </script>
 
