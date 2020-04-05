680ee3c08c9ed8331e95a43e037bd3c913547d2c
paulwoojungkim <wj2kim@gmail.com>
2020-04-05 13:03:33 +0900

    stackedbarchart

--- src/components/Chart.component.vue
+++ src/components/Chart.component.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="example">
-    <apexcharts width="500" height="350" type="bar" :options="chartOptions" :series="series"></apexcharts>
-  </div>
+    <div> 
+        <div>
+            <div class="example">
+                <apexcharts type="bar" height="350" :options="chartOptions" :series="series"></apexcharts>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -14,18 +18,70 @@ export default {
   },
   data: function() {
     return {
-      chartOptions: {
-        chart: {
-          id: 'basic-bar'
-        },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-        }
-      },
-      series: [{
-        name: 'series-1',
-        data: [30, 40, 45, 50, 49, 60, 70, 91]
-      }]
+    series: [{
+            name: 'Marine Sprite',
+            data: [44, 55, 41, 37, 22, 43, 21]
+          }, {
+            name: 'Striking Calf',
+            data: [53, 32, 33, 52, 13, 43, 32]
+          }, {
+            name: 'Tank Picture',
+            data: [12, 17, 11, 9, 15, 11, 20]
+          }, {
+            name: 'Bucket Slope',
+            data: [9, 7, 5, 8, 6, 9, 4]
+          }, {
+            name: 'Reborn Kid',
+            data: [25, 12, 19, 32, 25, 24, 10]
+          }],
+    chartOptions: {
+            chart: {
+              type: 'bar',
+              height: 350,
+              stacked: true,
+            },
+            plotOptions: {
+              bar: {
+                horizontal: true,
+              },
+            },
+            stroke: {
+              width: 1,
+              colors: ['#fff']
+            },
+            title: {
+              text: 'Fiction Books Sales'
+            },
+            xaxis: {
+              categories: [2008, 2009, 2010, 2011, 2012, 2013, 2014],
+              labels: {
+                formatter: function (val) {
+                  return val + "K"
+                }
+              }
+            },
+            yaxis: {
+              title: {
+                text: undefined
+              },
+            },
+            tooltip: {
+              y: {
+                formatter: function (val) {
+                  return val + "K"
+                }
+              }
+            },
+            fill: {
+              opacity: 1
+            },
+            legend: {
+              position: 'top',
+              horizontalAlign: 'left',
+              offsetX: 40
+            }
+          },
+
     }
   },
 }
--- src/main.js
+++ src/main.js
@@ -5,7 +5,5 @@ Vue.config.productionTip = false
 
 new Vue({
   el:"#app",
-  components : { App },
-  template : "<App/>",
   render: h => h(App),
 }).$mount('#app')
