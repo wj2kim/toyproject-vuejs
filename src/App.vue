fc451359e3891636fdf7d4fa3328fed2ee8a4b3a
paulwoojungkim <wj2kim@gmail.com>
2020-04-07 01:45:49 +0900

    two charts updated

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <h3>Toy_Project using Vue.js and Chart.js</h3>
-    <Chart/>
+    <StatisticsChart/>
   </div>
 </template>
 
 <script>
-import Chart from './components/StatisticsChart.vue'
+import StatisticsChart from './components/StatisticsChart.vue'
 
 export default {
   name: 'App',
   components: {
-    Chart
+    StatisticsChart
   }
 }
 </script>
